621e4fc0109c3d89ec072fc0a422823b7f18a756
Développeur A <gabrielpaul.pro@gmail.com>
2025-02-04 13:55:26 +0100

    Ajout du frontend

---
+++ script.js
@@ -0,0 +1,27 @@
+const socket = io("http://localhost:3000"); // Connexion au serveur
+socket.on("connect", () => {
+    console.log("Connecté au serveur !");
+});
+
+const chatBox = document.getElementById("chat-box");
+const messageInput = document.getElementById("message-input");
+const sendBtn = document.getElementById("send-btn");
+
+sendBtn.addEventListener("click", () => {
+    let message = messageInput.value.trim(); // Supprime les espaces
+    if (message !== "") {
+        console.log("Envoi du message :", message);
+        socket.emit("message", message);
+        messageInput.value = ""; // Vide le champ après l'envoi
+    }
+});
+
+
+socket.on("message", (msg) => {
+    console.log("Message reçu :", msg);
+    const msgElement = document.createElement("p");
+    msgElement.textContent = msg;
+    chatBox.appendChild(msgElement);
+    chatBox.scrollTop = chatBox.scrollHeight;
+});
+
